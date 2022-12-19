2d05759777d5348388c6b8a4fd88611769034ac1
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-12-19 18:43:18 +0100

    get the example to compile and run

--- build.sh
+++ build.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-PATH=$PATH:$ANDROID_HOME/build-tools/28.0.3
-PATH=$PATH:$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
+export ANDROID_HOME=/home/martin/quest2/sdk
+PATH=$PATH:$ANDROID_HOME/build-tools/29.0.3
+PATH=$PATH:/home/martin/quest2/ndk/android-ndk-r25b/toolchains/llvm/prebuilt/linux-x86_64/bin
+export NDK_HOME=/home/martin/quest2/ndk/android-ndk-r25b
+export OVR_HOME=/home/martin/quest2/ovr/
 
 rm -rf build
 mkdir -p build
 pushd build > /dev/null
 javac\
-	-classpath $ANDROID_HOME/platforms/android-26/android.jar\
+	-classpath $ANDROID_HOME/platforms/android-29/android.jar\
 	-d .\
 	../src/main/java/com/makepad/hello_quest/*.java
 dx --dex --output classes.dex .
 mkdir -p lib/arm64-v8a
 pushd lib/arm64-v8a > /dev/null
-aarch64-linux-android26-clang\
+aarch64-linux-android29-clang\
     -march=armv8-a\
     -shared\
     -I $NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/\
     -I $OVR_HOME/VrApi/Include\
-    -L $NDK_HOME/platforms/android-26/arch-arm64/usr/lib\
+    -L $NDK_HOME/platforms/android-29/arch-arm64/usr/lib\
     -L $OVR_HOME/VrApi/Libs/Android/arm64-v8a/Debug\
     -landroid\
     -llog\
     -lvrapi\
+    -lEGL \
+    -lGLESv3 \
     -o libmain.so\
    ../../../src/main/cpp/*.c
 cp $OVR_HOME/VrApi/Libs/Android/arm64-v8a/Debug/libvrapi.so .
@@ -29,7 +34,7 @@ popd > /dev/null
 aapt\
 	package\
 	-F hello_quest.apk\
-	-I $ANDROID_HOME/platforms/android-26/android.jar\
+	-I $ANDROID_HOME/platforms/android-29/android.jar\
 	-M ../src/main/AndroidManifest.xml\
 	-f
 aapt add hello_quest.apk classes.dex
